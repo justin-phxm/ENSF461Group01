b6138fb9bdeffca4bf35e3e3755247cfc09bafd4
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-09-26 14:45:58 -0600

    formatting code with prettier

--- lab03-template/lab03-Group01/parser.c
+++ lab03-template/lab03-Group01/parser.c
@@ -1,76 +1,83 @@
 #include "parser.h"
 
-//Command to trim whitespace and ASCII control characters from buffer
+// Command to trim whitespace and ASCII control characters from buffer
 //[Input] char* inputbuffer - input string to trim
 //[Input] size_t bufferlen - size of input and output string buffers
-//[Output] char* outputbuffer - output string after trimming 
+//[Output] char* outputbuffer - output string after trimming
 //[Return] size_t - size of output string after trimming
-size_t trimstring(char* outputbuffer, const char* inputbuffer, size_t bufferlen)
-{   
-    memcpy(outputbuffer, inputbuffer, bufferlen*sizeof(char));
+size_t trimstring(char *outputbuffer, const char *inputbuffer, size_t bufferlen)
+{
+    memcpy(outputbuffer, inputbuffer, bufferlen * sizeof(char));
 
-    for(size_t ii = strlen(outputbuffer)-1; ii >=0; ii--){
-        if(outputbuffer[ii] < '!') //In ASCII '!' is the first printable (non-control) character
+    for (size_t ii = strlen(outputbuffer) - 1; ii >= 0; ii--)
+    {
+        if (outputbuffer[ii] < '!') // In ASCII '!' is the first printable (non-control) character
         {
             outputbuffer[ii] = 0;
-        }else{
+        }
+        else
+        {
             break;
-        }    
+        }
     }
 
     return strlen(outputbuffer);
 }
 
-//Command to trim the input command to just be the first word
+// Command to trim the input command to just be the first word
 //[Input] char* inputbuffer - input string to trim
 //[Input] size_t bufferlen - size of input and output string buffers
-//[Output] char* outputbuffer - output string after trimming 
+//[Output] char* outputbuffer - output string after trimming
 //[Return] size_t - size of output string after trimming
-size_t firstword(char* outputbuffer, const char* inputbuffer, size_t bufferlen)
+size_t firstword(char *outputbuffer, const char *inputbuffer, size_t bufferlen)
 {
-    //TO DO: Implement this function
+    // TO DO: Implement this function
     return 0;
 }
 
-//Command to test that string only contains valid ascii characters (non-control and not extended)
+// Command to test that string only contains valid ascii characters (non-control and not extended)
 //[Input] char* inputbuffer - input string to test
 //[Input] size_t bufferlen - size of input buffer
 //[Return] bool - true if no invalid ASCII characters present
-bool isvalidascii(const char* inputbuffer, size_t bufferlen)
+bool isvalidascii(const char *inputbuffer, size_t bufferlen)
 {
-    //TO DO: Correct this function so that the second test string fails
+    // TO DO: Correct this function so that the second test string fails
     size_t testlen = bufferlen;
     size_t stringlength = strlen(inputbuffer);
-    if(strlen(inputbuffer) < bufferlen){
+    if (strlen(inputbuffer) < bufferlen)
+    {
         testlen = stringlength;
     }
 
     bool isValid = true;
-    for(size_t ii = 0; ii < testlen; ii++)
+    for (size_t ii = 0; ii < testlen; ii++)
     {
-        isValid &= ((unsigned char) inputbuffer[ii] <= '~'); //In (lower) ASCII '~' is the last printable character
-    }
+        isValid &= ((unsigned char)inputbuffer[ii] <= '~'); // In (lower) ASCII '~' is the last printable character
+        isValid &= ((unsigned char)inputbuffer[ii] >= ' '); // In (lower) ASCII ' ' is the first printable character
+        }
 
     return isValid;
 }
 
-//Command to find location of pipe character in input string
+// Command to find location of pipe character in input string
 //[Input] char* inputbuffer - input string to test
 //[Input] size_t bufferlen - size of input buffer
 //[Return] int - location in the string of the pipe character, or -1 pipe character not found
-int findpipe(const char* inputbuffer, size_t bufferlen){
-    //TO DO: Implement this function
+int findpipe(const char *inputbuffer, size_t bufferlen)
+{
+    // TO DO: Implement this function
 
     return -1;
 }
 
-//Command to test whether the input string ends with "&" and
-//thus represents a command that should be run in background
+// Command to test whether the input string ends with "&" and
+// thus represents a command that should be run in background
 //[Input] char* inputbuffer - input string to test
 //[Input] size_t bufferlen - size of input buffer
 //[Return] bool - true if string ends with "&"
-bool runinbackground(const char* inputbuffer, size_t bufferlen){
-    //TO DO: Implement this function
+bool runinbackground(const char *inputbuffer, size_t bufferlen)
+{
+    // TO DO: Implement this function
 
     return false;
 }
