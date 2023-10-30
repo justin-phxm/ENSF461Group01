3394068593b3f50483fa6674e80a335eea178cd3
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-10-30 14:21:33 -0600

    work on test2

--- lab6-template/myalloc.c
+++ lab6-template/myalloc.c
@@ -60,6 +60,9 @@ int myinit(size_t size)
     fprintf(stderr, "...arena ends at %p\n", _arena_start + arenaSize);
     fprintf(stderr, "...initializing header for initial free chunk\n");
     // todo: initialize header for initial free chunk
+    node_t *header = (node_t *)_arena_start;
+    header->size = arenaSize - sizeof(node_t);
+    header->is_free = 1;
     fprintf(stderr, "...header size is 32 bytes\n");
 
     return arenaSize;
@@ -90,25 +93,30 @@ void *myalloc(size_t size)
     }
     fprintf(stderr, "Allocating memory:\n");
     fprintf(stderr, "...looking for free chunk of >= %lu bytes\n", size);
-    if (size > arenaSize - sizeof(node_t))
+    node_t *header = (node_t *)_arena_start;
+    while (header->is_free == 0 && header->size < size)
+    {
+        header = header->fwd;
+    }
+    if (header->size < size)
     {
         fprintf(stderr, "...no such free chunk exists\n");
         fprintf(stderr, "...setting error code\n");
         statusno = ERR_OUT_OF_MEMORY;
         return NULL;
     }
-    fprintf(stderr, "...found free chunk of %lu bytes with header at %p\n", size, _arena_start);
-    fprintf(stderr, "...free chunk->fwd currently points to (%p)\n", ((node_t *)_arena_start)->fwd);
-    fprintf(stderr, "...free chunk->bwd currently points to (%p)\n", ((node_t *)_arena_start)->bwd);
+    fprintf(stderr, "...found free chunk of %lu bytes with header at %p\n", header->size, header);
+    fprintf(stderr, "...free chunk->fwd currently points to %p\n", header->fwd);
+    fprintf(stderr, "...free chunk->bwd currently points to %p\n", header->bwd);
     fprintf(stderr, "...checking if splitting is required\n");
     // todo: check if splitting is required
     if (1)
     {
         fprintf(stderr, "...splitting is not required\n");
     }
-    fprintf(stderr, "...updating chunk header at %p\n", _arena_start);
+    fprintf(stderr, "...updating chunk header at %p\n", header);
     fprintf(stderr, "...being careful with my pointer arithmetic and void pointer casting\n");
-    fprintf(stderr, "...allocation starts at %p\n", _arena_start + sizeof(node_t));
+    fprintf(stderr, "...allocation starts at %p\n", header + sizeof(node_t));
     return _arena_start;
 }
 
