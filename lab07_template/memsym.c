7308cb737aca6d2bd4601c1ab1412911c49a8c58
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-11-05 14:17:55 -0700

    updated to test 3.3

--- lab07_template/memsym.c
+++ lab07_template/memsym.c
@@ -6,7 +6,7 @@
 #define TRUE 1
 #define FALSE 0
 #define TLB_SIZE 8
-
+#define PAGE_TABLE_SIZE 4
 // Forward declarations of structs
 struct TLB_entry;
 struct Page_table_entry;
@@ -18,6 +18,9 @@ typedef struct
     unsigned int vpn;
     unsigned int ppn;
     int valid; // validity bit: can be 0 (invalid) or 1 (valid)
+    unsigned int last_access_time;
+    unsigned int pid;
+
 } TLB_entry;
 
 typedef struct
@@ -45,7 +48,7 @@ unsigned int size_offset, size_ppn, size_vpn, current_pid = 0;
 unsigned long int **physical_memory = NULL;
 unsigned long int r1 = 0, r2 = 0;
 TLB_entry TLB[TLB_SIZE];
-Page_table pt[4];
+Page_table pt[PAGE_TABLE_SIZE];
 
 // Function prototypes
 void initialize_TLB();
@@ -55,13 +58,18 @@ void map(unsigned int vpn, unsigned int ppn);
 void unmap(unsigned int vpn);
 void pinspect(unsigned int vpn);
 TLB_entry tinspect(unsigned int tlbn);
-void load(char *dst, unsigned int src);
+void load(char *dst, char *src);
 void add();
+// void store(unsigned int dst, unsigned int src);
+void store(unsigned int dst, char *src);
 
 // unsigned long int linspect();
 void initialize_TLB();
 void initialize_physical_memory(unsigned int num_offset_bits, unsigned int num_ppn_bits);
 void cleanup_physical_memory();
+unsigned int VPN_to_PFN_address_translation(unsigned int vpn, unsigned int offset);
+void add_TLB_entry(unsigned int vpn, unsigned int ppn);
+void remove_TLB_entry(unsigned int vpn);
 
 char **tokenize_input(char *input)
 {
@@ -172,8 +180,7 @@ int main(int argc, char *argv[])
                 }
                 else if (strcmp(tokens[0], "load") == 0)
                 {
-                    char *skipHash = tokens[2] + 1;
-                    load(tokens[1], atoi(skipHash));
+                    load(tokens[1], tokens[2]);
                 }
                 else if (strcmp(tokens[0], "add") == 0)
                 {
@@ -186,6 +193,10 @@ int main(int argc, char *argv[])
                 else if (strcmp(tokens[0], "unmap") == 0)
                 {
                     unmap(atoi(tokens[1]));
+                }
+                else if (strcmp(tokens[0], "store") == 0)
+                {
+                    store(atoi(tokens[1]), tokens[2]);
                 }
                 else
                 {
@@ -212,22 +223,60 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void load(char *dst, unsigned int src)
+void load(char *dst, char *src)
 {
-    if (strcmp(dst, "r1") == 0)
+    unsigned int virtualAddress;
+    // If src is an immediate
+    char possibleHash[2];
+    possibleHash[0] = src[0];
+    possibleHash[1] = '\0';
+    if (strcmp(possibleHash, "#") == 0)
     {
-        r1 = src;
-        fprintf(output_file, "Loaded immediate %d into register %s\n", src, dst);
+        // Move src to the right by 1
+        src = src + 1;
+        int immediate = atoi(src);
+        if (strcmp(dst, "r1") == 0)
+        {
+            r1 = immediate;
+            fprintf(output_file, "Loaded immediate %d into register %s\n", immediate, dst);
+        }
+        else if (strcmp(dst, "r2") == 0)
+        {
+            r2 = immediate;
+            fprintf(output_file, "Loaded immediate %d into register %s\n", immediate, dst);
+        }
+        else
+        {
+            fprintf(output_file, "Error: invalid register operand %s\n", dst);
+            exit(1);
+        }
     }
-    else if (strcmp(dst, "r2") == 0)
-    {
-        r2 = src;
-        fprintf(output_file, "Loaded immediate %d into register %s\n", src, dst);
-    }
+    // If src is a memory location
     else
     {
-        fprintf(output_file, "Error: invalid register operand %s\n", dst);
+        virtualAddress = atoi(src);
+
+        // Check the TLB for the physical address
+        unsigned int physicalAddress = VPN_to_PFN_address_translation(virtualAddress, 0);
+        if (strcmp(dst, "r1") == 0)
+        {
+            r1 = physical_memory[physicalAddress][0];
+            fprintf(output_file, "Loaded value of location %s (%ld) into register %s\n", src, r1, dst);
+        }
+        else if (strcmp(dst, "r2") == 0)
+        {
+            r2 = physical_memory[physicalAddress][0];
+            fprintf(output_file, "Loaded value of location %s (%ld) into register %s\n", src, r2, dst);
+        }
+        else
+        {
+            fprintf(output_file, "Error: invalid register operand %s\n", dst);
+            exit(1);
+        }
     }
+
+    // If src is a memory location, add to TLB
+    // add_TLB_entry(src, src);
 }
 
 // Initialize TLB entries
@@ -238,6 +287,7 @@ void initialize_TLB()
         TLB[i].vpn = 0;
         TLB[i].ppn = 0;
         TLB[i].valid = 0;
+        TLB[i].last_access_time = 0;
     }
 }
 
@@ -256,6 +306,7 @@ void initialize_physical_memory(unsigned int num_offset_bits, unsigned int num_p
     }
 
     // Initialize each word in memory to 0
+
     for (int i = 0; i < num_ppn_bits; i++)
     {
         physical_memory[i] = (unsigned long int *)calloc(pageSize, sizeof(unsigned long int));
@@ -271,7 +322,7 @@ void initialize_physical_memory(unsigned int num_offset_bits, unsigned int num_p
 void initialize_Page_table(unsigned int num_vpn_bits)
 {
     int numVirtualPages = pow(2, num_vpn_bits);
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < PAGE_TABLE_SIZE; i++)
     {
         pt[i].entries = (Page_table_entry *)malloc(numVirtualPages * sizeof(Page_table_entry));
         pt[i].r1 = 0;
@@ -294,6 +345,88 @@ void cleanup_physical_memory(int size)
     }
     free(physical_memory);
 }
+
+void add_TLB_entry(unsigned int vpn, unsigned int ppn)
+
+{
+    if (strcmp(strategy, "FIFO") == 0)
+    {
+        // todo: implement FIFO strategy
+        for (int i = 0; i < TLB_SIZE; i++)
+        {
+            if (TLB[i].valid == 0)
+            {
+                TLB[i].vpn = vpn;
+                TLB[i].ppn = ppn;
+                TLB[i].valid = 1;
+                TLB[i].pid = current_pid;
+                // fprintf(output_file, "Lookup for VPN %ld hit in TLB entry %ld. PFN is %ld", vpn, i, ppn);
+                return;
+            }
+        }
+    }
+    else if (strcmp(strategy, "LRU") == 0)
+    {
+        // todo: implement LRU strategy
+        for (int i = 0; i < TLB_SIZE; i++)
+        {
+            if (TLB[i].valid == 0)
+            {
+                TLB[i].vpn = vpn;
+                TLB[i].ppn = ppn;
+                TLB[i].valid = 1;
+                // fprintf(output_file, "Lookup for VPN %ld hit in TLB entry %ld. PFN is %ld", vpn, i, ppn);
+                return;
+            }
+        }
+    }
+    else
+    {
+        fprintf(output_file, "Error: invalid TLB replacement strategy %s\n", strategy);
+        exit(1);
+    }
+}
+
+void remove_TLB_entry(unsigned int vpn)
+{
+    for (int i = 0; i < TLB_SIZE; i++)
+    {
+        if (TLB[i].vpn == vpn)
+        {
+            TLB[i].valid = 0;
+            return;
+        }
+    }
+};
+
+unsigned int VPN_to_PFN_address_translation(unsigned int vpn, unsigned int offset)
+{
+    fprintf(output_file, "Translating. ");
+
+    // Temp solution
+    for (int i = 0; i < TLB_SIZE; i++)
+    {
+        if (TLB[i].valid == 1 && TLB[i].pid == current_pid)
+        {
+            fprintf(output_file, "Lookup for VPN %d hit in TLB entry %d. PFN is %d\nCurrent PID: %d. ", TLB[i].vpn, i, TLB[i].ppn, current_pid);
+            return TLB[i].ppn;
+        }
+    }
+    // Check if vpn is in TLB
+    // for (int i = 0; i < TLB_SIZE; i++)
+    // {
+    //     if (TLB[i].vpn == vpn && TLB[i].valid == 1)
+    //     {
+    //         fprintf(output_file, "Lookup for VPN %d hit in TLB entry %d. PFN is %d\n", vpn, i, TLB[i].ppn);
+    //         return TLB[i].ppn;
+    //     }
+    // }
+
+    // Handle cache miss
+
+    return 0;
+}
+
 // Function to set global variables
 void define(unsigned int num_offset_bits, unsigned int num_ppn_bits, unsigned int num_vpn_bits)
 {
@@ -332,6 +465,9 @@ void map(unsigned int vpn, unsigned int ppn)
 {
     int numVirtualPageNumbers = pow(2, size_vpn);
 
+    // Add to TLB
+    add_TLB_entry(vpn, ppn);
+
     // check if vpn already exists in page table
     for (int i = 0; i < numVirtualPageNumbers; i++)
     {
@@ -343,6 +479,7 @@ void map(unsigned int vpn, unsigned int ppn)
         }
     }
 
+    // Otherwise, add it to the page table
     for (int i = 0; i < numVirtualPageNumbers; i++)
     {
         if (pt[current_pid].entries[i].valid == 0)
@@ -357,6 +494,7 @@ void map(unsigned int vpn, unsigned int ppn)
 void unmap(unsigned int vpn)
 {
     int numVirtualPageNumbers = pow(2, size_vpn);
+    remove_TLB_entry(vpn);
     for (int i = 0; i < numVirtualPageNumbers; i++)
     {
         if (pt[current_pid].entries[i].vpn == vpn)
@@ -372,4 +510,29 @@ void add()
     unsigned long int sum = r1 + r2;
     fprintf(output_file, "Added contents of registers r1 (%ld) and r2 (%ld). Result: %ld\n", r1, r2, sum);
     r1 = sum;
+}
+// void store(unsigned int dst, unsigned int src)
+// {
+//     return;
+// }
+
+void store(unsigned int dst, char *src)
+{
+    unsigned int physicalAddress = VPN_to_PFN_address_translation(dst, 0);
+    if (strcmp(src, "r1") == 0)
+    {
+        fprintf(output_file, "Stored value of register %s (%ld) into location %d\n", src, r1, dst);
+        physical_memory[physicalAddress][0] = r1;
+    }
+    else if (strcmp(src, "r2") == 0)
+    {
+        fprintf(output_file, "Stored value of register %s (%ld) into location %d\n", src, r2, dst);
+        physical_memory[physicalAddress][0] = r2;
+    }
+    else
+    {
+        fprintf(output_file, "Error: invalid register operand %s\n", src);
+        exit(1);
+    }
+    return;
 }
