45ef4317d1e4518557138dfa7e0a0509e40aac20
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2023-11-07 15:14:08 -0700

    Co-authored-by: Joshua Jipp <joshuajipp@users.noreply.github.com> Co-authored-by: Zuriel Abasola <ZurielAbasola@users.noreply.github.com>

--- lab08_template/prefixscan.c
+++ lab08_template/prefixscan.c
@@ -1,42 +1,188 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 
+typedef struct
+{
+    int *data;
+    int step;
+    int start;
+    int end;
+} thread_args;
+
+void *up_sweep_thread(void *arg)
+{
+    thread_args *args = (thread_args *)arg;
+    int start = args->start;
+    int end = args->end;
+    int step = args->step;
+
+    for (int i = start; i < end; i += step)
+    {
+        args->data[i + step - 1] += args->data[i + (step / 2) - 1];
+    }
+
+    pthread_exit(NULL);
+}
+
+void *down_sweep_thread(void *arg)
+{
+    thread_args *args = (thread_args *)arg;
+    int start = args->start;
+    int end = args->end;
+    int step = args->step;
+
+    for (int i = start; i < end; i += step)
+    {
+        int t = args->data[i + (step / 2) - 1];
+        args->data[i + (step / 2) - 1] = args->data[i + step - 1];
+        args->data[i + step - 1] += t;
+    }
+
+    pthread_exit(NULL);
+}
+
+void parallel_scan(int *data, int length, int num_threads)
+{
+    pthread_t threads[num_threads];
+    thread_args args[num_threads];
+
+    // Up-sweep phase
+    for (int d = 0; d < log2(length); ++d)
+    {
+        int step = pow(2, d + 1);
+        for (int i = 0; i < num_threads; ++i)
+        {
+            args[i].data = data;
+            args[i].step = step;
+            args[i].start = i * step;
+            args[i].end = fmin(length, (i + 1) * step);
+            if (pthread_create(&threads[i], NULL, up_sweep_thread, &args[i]))
+            {
+                fprintf(stderr, "Error creating thread\n");
+                exit(1);
+            }
+        }
+        for (int i = 0; i < num_threads; ++i)
+        {
+            pthread_join(threads[i], NULL);
+        }
+    }
+
+    data[length - 1] = 0; // Set last element to zero for exclusive scan
+
+    // Down-sweep phase
+    for (int d = log2(length) - 1; d >= 0; --d)
+    {
+        int step = pow(2, d + 1);
+        for (int i = 0; i < num_threads; ++i)
+        {
+            args[i].data = data;
+            args[i].step = step;
+            args[i].start = i * step;
+            args[i].end = fmin(length, (i + 1) * step);
+            if (pthread_create(&threads[i], NULL, down_sweep_thread, &args[i]))
+            {
+                fprintf(stderr, "Error creating thread\n");
+                exit(1);
+            }
+        }
+        for (int i = 0; i < num_threads; ++i)
+        {
+            pthread_join(threads[i], NULL);
+        }
+    }
+}
 
 // Parse a vector of integers from a file, one per line.
 // Return the number of integers parsed.
-int parse_ints(FILE *file, int **ints) {
+int parse_ints(FILE *file, int **ints)
+{
+    int value;
+    int size = 0; // Number of ints stored.
 
+    // Allocate memory for the array dynamically.
+    *ints = NULL;
+
+    // Read integers one by one.
+    while (fscanf(file, "%d", &value) == 1)
+    {
+        // Reallocate the array to make room for the new integer.
+        int *temp = realloc(*ints, (size + 1) * sizeof(int));
+        if (!temp)
+        {
+            perror("Realloc failed");
+            free(*ints); // Free any memory that was allocated.
+            return -1;
+        }
+        *ints = temp;
+        (*ints)[size++] = value; // Store the integer.
+    }
+
+    return size; // Return the number of elements read.
 }
-
 
 // Write a vector of integers to a file, one per line.
-void write_ints(FILE *file, int *ints, int size) {
+void write_ints(FILE *file, const int *ints, int size)
+{
+    if (file == NULL || ints == NULL)
+    {
+        fprintf(stderr, "Invalid file pointer or array reference.\n");
+        return;
+    }
 
+    for (int i = 0; i < size; ++i)
+    {
+        fprintf(file, "%d\n", ints[i]);
+    }
 }
-
 
 // Return the result of a sequential prefix scan of the given vector of integers.
-int* SEQ(int *ints, int size) {
-
+int *SEQ(int *ints, int size)
+{
+    int *prefixScan = (int *)malloc(size * sizeof(int));
+    prefixScan[0] = ints[0];
+    for (int i = 1; i < size; i++)
+    {
+        prefixScan[i] = ints[i] + prefixScan[i - 1];
+    }
+    return prefixScan;
 }
-
 
 // Return the result of Hillis/Steele, but with each pass executed sequentially
-int* HSS(int *ints, int size) {
+int *HSS(int *ints, int size)
+{
+    int *prefixScan = (int *)malloc(size * sizeof(int));
 
+    // Initialize prefixScan and other arrays
+
+    // Split the input array into two halves
+
+    // Create auxiliary arrays for the prefix scan
+
+    // Perform parallel prefix scan using multithreading or parallel processing
+
+    // Combine the results from both halves to get the final prefix scan result
+
+    // Return the prefixScan array
+
+    return prefixScan;
 }
-
-
 // Return the result of Hillis/Steele, parallelized using pthread
-int* HSP(int *ints, int size, int numthreads) {
+int *HSP(int *ints, int size, int numthreads)
+{
+    int *prefixScan = (int *)malloc(size * sizeof(int));
+    parallel_scan(prefixScan, size, numthreads);
 
+    return prefixScan;
 }
 
+int main(int argc, char **argv)
+{
 
-int main(int argc, char** argv) {
-
-    if ( argc != 5 ) {
+    if (argc != 5)
+    {
         printf("Usage: %s <mode> <#threads> <input file> <output file>\n", argv[0]);
         return 1;
     }
@@ -49,15 +195,23 @@ int main(int argc, char** argv) {
     int *ints;
     int size;
     size = parse_ints(input, &ints);
+    // ints[] looks like [1, 2, 3, 4]
 
     int *result;
-    if (strcmp(mode, "SEQ") == 0) {
+    if (strcmp(mode, "SEQ") == 0)
+    {
         result = SEQ(ints, size);
-    } else if (strcmp(mode, "HSS") == 0) {
+    }
+    else if (strcmp(mode, "HSS") == 0)
+    {
         result = HSS(ints, size);
-    } else if (strcmp(mode, "HSP") == 0) {
+    }
+    else if (strcmp(mode, "HSP") == 0)
+    {
         result = HSP(ints, size, num_threads);
-    } else {
+    }
+    else
+    {
         printf("Unknown mode: %s\n", mode);
         return 1;
     }
